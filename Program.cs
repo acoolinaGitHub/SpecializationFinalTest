3667386ebd583f534274495633b63c407aae350c
Ina Bern <acoolina@gmail.com>
2023-03-21 17:09:17 +0300

    1.0 realise (C#) of Ina Bern

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// create new str array from str array with lenght of array[i] <= 3  
+
+Console.Clear();
+Console.Write("Please enter string separated by spaces --> ");
+string str = Console.ReadLine();
+
+string[] arr = str.Split(' ');
+
+string[] newArr = new string[arr.Length];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if ((arr[i].Length < 3) || (arr[i].Length == 3))
+    {
+        newArr[i] = arr[i];
+    }    
+}
+
+Console.Write("[");
+foreach (var item in newArr)
+{
+    Console.Write($"{item} ");
+}
+Console.Write("]");
+
